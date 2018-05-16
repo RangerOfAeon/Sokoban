1243cda86aff251723500f8c1e453565a1abc637
RangerOfAeon <is1997@hotmail.com>
2018-05-16 14:36:40 +0200

    Nu kan man se något

--- Java.js
+++ Java.js
@@ -1,154 +1,154 @@
-// JavaScript source code
+//// JavaScript source code
 
-var gamePlayer;
-var gameBlocks = [];
-var gameGoals = [];
-var Continue = true;
+//var gamePlayer;
+//var gameBlocks = [];
+//var gameGoals = [];
+//var Continue = true;
 
-function startGame() {
-    startSoko.start();
-}
+//function startGame() {
+//    startSoko.start();
+//}
 
-var startSoko = {
-    canvas: document.createElement("canvas"),
-    start: function () {
-        this.canvas.width = tileMap.length * 30;
-        this.canvas.height = tileMap.length * 30;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 20);
+//var startSoko = {
+//    canvas: document.createElement("canvas"),
+//    start: function () {
+//        this.canvas.width = tileMap.length * 30;
+//        this.canvas.height = tileMap.length * 30;
+//        this.context = this.canvas.getContext("2d");
+//        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+//        this.interval = setInterval(updateGameArea, 20);
 
-            for (index = 0; tileMap.mapGrid.length; index++) {
-                for (index2 = 0; tileMap.mapGrid[index].length; index2++) {
-                    switch (tileMap.mapGrid[index][index2].toString()) {
-                        case "W":
-                            gameBlocks.push(new allBlocks(30, 30, "green", index * 30, index2 * 30, index2, index, "wall"));
-                            break;
-                        case "G":
-                            gameGoals.push(new allBlocks(30, 30, "orange", index * 30, index2 * 30, index2, index, "goal"));
-                            gameGoals += 1;
-                            break;
-                        case "B":
-                            gameBlocks.push(new allBlocks(30, 30, "blue", indexm * 30, index2 * 30, index2, index, "move"));
-                            break;
-                        case "P":
-                            player = new gamePlayer(30, 30, "red", index * 30, index2 * 30, imdex2, index);
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            }
-        window.addEventListener('keydown', function (e) {
-            startSoko.key = e.keyCode;
-        })
-        window.addEventListener('keyup', function (e) {
-            startSoko.key = false;
-        })
-    }
-}
-function component(width, height, color, x, y) {
-    this.width = width;
-    this.height = height;
-    this.speedX = 0;
-    this.speedY = 0;
-    this.x = x;
-    this.y = y;
-    this.update = function () {
-         ctx = startGame.context;
-         ctx.fillStyle = color;
-         ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-    this.newPos = function () {
-        this.x += this.speedX;
-        this.y += this.speedY;
-    }
-    this.crashWith = function (otherobj) {
-        var myleft = this.x;
-        var myright = this.x + (this.width);
-        var mytop = this.y;
-        var mybottom = this.y + (this.height);
-        var otherleft = otherobj.x;
-        var otherright = otherobj.x + (otherobj.width);
-        var othertop = otherobj.y;
-        var otherbottom = otherobj.y + (otherobj.height);
-        var crash = true;
-        if ((mybottom < othertop) ||
-            (mytop > otherbottom) ||
-            (myright < otherleft) ||
-            (myleft > otherright)) {
-            crash = false;
-        }
-        return crash;
-    }
-}
-function updateGameArea() {
-    //if (gamePlayer.crashWith(gameWall)) {
-    //    if (mybottom < othertop) {
-    //        if (startGame.key && startGame.key == 37) { gamePlayer.speedX = -30; }
-    //        if (startGame.key && startGame.key == 39) { gamePlayer.speedX = 30; }
-    //        if (startGame.key && startGame.key == 38) { gamePlayer.speedY = -30; }
-    //        if (startGame.key && startGame.key == 40) { gamePlayer.speedY = 0; }
-    //    }
-    //    else if (mytop < otherbottom) {
-    //        if (startGame.key && startGame.key == 37) { gamePlayer.speedX = -30; }
-    //        if (startGame.key && startGame.key == 39) { gamePlayer.speedX = 30; }
-    //        if (startGame.key && startGame.key == 38) { gamePlayer.speedY = 0; }
-    //        if (startGame.key && startGame.key == 40) { gamePlayer.speedY = 30; }
-    //    }
-    //    else if (myright < otherleft) {
-    //        if (startGame.key && startGame.key == 37) { gamePlayer.speedX = -30; }
-    //        if (startGame.key && startGame.key == 39) { gamePlayer.speedX = 0; }
-    //        if (startGame.key && startGame.key == 38) { gamePlayer.speedY = -30; }
-    //        if (startGame.key && startGame.key == 40) { gamePlayer.speedY = 30; }
-    //    }
-    //    else if (myleft < otherright) {
-    //        if (startGame.key && startGame.key == 37) { gamePlayer.speedX = 0; }
-    //        if (startGame.key && startGame.key == 39) { gamePlayer.speedX = 30; }
-    //        if (startGame.key && startGame.key == 38) { gamePlayer.speedY = -30; }
-    //        if (startGame.key && startGame.key == 40) { gamePlayer.speedY = 30; }
-    //    }
-    //} else {
-        startGame.clear();
-        gameWall.update();
-        gamePlayer.speedX = 0;
-        gamePlayer.speedY = 0;
-        if (startGame.key && startGame.key == 37) { gamePlayer.speedX = -30; }
-        if (startGame.key && startGame.key == 39) { gamePlayer.speedX = 30; }
-        if (startGame.key && startGame.key == 38) { gamePlayer.speedY = -30; }
-        if (startGame.key && startGame.key == 40) { gamePlayer.speedY = 30; }
-
-        gamePlayer.newPos();
-        gamePlayer.update();
-    //}
-}
-function moveUp() {
-    gamePlayer.speedY -= 30;
-} 
-
-function moveDown() {
-    gamePlayer.speedY += 30;
-}
-
-function moveLeft() {
-    gamePlayer.speedX -= 30;
-}
-
-function moveRight() {
-    gamePlayer.speedX += 30;
-}
-function stopMove() {
-    gamePlayer.speedX = 0;
-    gamePlayer.speedY = 0;
-}
-function Collide() {
-    while (true) {
-        if (!collide(gamePlayer, gameWall))
-            gamePlayer.x++;
-    }
-}
-//function Controll() {
-//    while (Continue == true) {
-//        if()
+//            for (index = 0; tileMap.mapGrid.length; index++) {
+//                for (index2 = 0; tileMap.mapGrid[index].length; index2++) {
+//                    switch (tileMap.mapGrid[index][index2].toString()) {
+//                        case "W":
+//                            gameBlocks.push(new allBlocks(30, 30, "green", index * 30, index2 * 30, index2, index, "wall"));
+//                            break;
+//                        case "G":
+//                            gameGoals.push(new allBlocks(30, 30, "orange", index * 30, index2 * 30, index2, index, "goal"));
+//                            gameGoals += 1;
+//                            break;
+//                        case "B":
+//                            gameBlocks.push(new allBlocks(30, 30, "blue", indexm * 30, index2 * 30, index2, index, "move"));
+//                            break;
+//                        case "P":
+//                            player = new gamePlayer(30, 30, "red", index * 30, index2 * 30, imdex2, index);
+//                            break;
+//                        default:
+//                            break;
+//                    }
+//                }
+//            }
+//        window.addEventListener('keydown', function (e) {
+//            startSoko.key = e.keyCode;
+//        })
+//        window.addEventListener('keyup', function (e) {
+//            startSoko.key = false;
+//        })
 //    }
 //}
+//function component(width, height, color, x, y) {
+//    this.width = width;
+//    this.height = height;
+//    this.speedX = 0;
+//    this.speedY = 0;
+//    this.x = x;
+//    this.y = y;
+//    this.update = function () {
+//         ctx = startGame.context;
+//         ctx.fillStyle = color;
+//         ctx.fillRect(this.x, this.y, this.width, this.height);
+//    }
+//    this.newPos = function () {
+//        this.x += this.speedX;
+//        this.y += this.speedY;
+//    }
+//    this.crashWith = function (otherobj) {
+//        var myleft = this.x;
+//        var myright = this.x + (this.width);
+//        var mytop = this.y;
+//        var mybottom = this.y + (this.height);
+//        var otherleft = otherobj.x;
+//        var otherright = otherobj.x + (otherobj.width);
+//        var othertop = otherobj.y;
+//        var otherbottom = otherobj.y + (otherobj.height);
+//        var crash = true;
+//        if ((mybottom < othertop) ||
+//            (mytop > otherbottom) ||
+//            (myright < otherleft) ||
+//            (myleft > otherright)) {
+//            crash = false;
+//        }
+//        return crash;
+//    }
+//}
+//function updateGameArea() {
+//    //if (gamePlayer.crashWith(gameWall)) {
+//    //    if (mybottom < othertop) {
+//    //        if (startGame.key && startGame.key == 37) { gamePlayer.speedX = -30; }
+//    //        if (startGame.key && startGame.key == 39) { gamePlayer.speedX = 30; }
+//    //        if (startGame.key && startGame.key == 38) { gamePlayer.speedY = -30; }
+//    //        if (startGame.key && startGame.key == 40) { gamePlayer.speedY = 0; }
+//    //    }
+//    //    else if (mytop < otherbottom) {
+//    //        if (startGame.key && startGame.key == 37) { gamePlayer.speedX = -30; }
+//    //        if (startGame.key && startGame.key == 39) { gamePlayer.speedX = 30; }
+//    //        if (startGame.key && startGame.key == 38) { gamePlayer.speedY = 0; }
+//    //        if (startGame.key && startGame.key == 40) { gamePlayer.speedY = 30; }
+//    //    }
+//    //    else if (myright < otherleft) {
+//    //        if (startGame.key && startGame.key == 37) { gamePlayer.speedX = -30; }
+//    //        if (startGame.key && startGame.key == 39) { gamePlayer.speedX = 0; }
+//    //        if (startGame.key && startGame.key == 38) { gamePlayer.speedY = -30; }
+//    //        if (startGame.key && startGame.key == 40) { gamePlayer.speedY = 30; }
+//    //    }
+//    //    else if (myleft < otherright) {
+//    //        if (startGame.key && startGame.key == 37) { gamePlayer.speedX = 0; }
+//    //        if (startGame.key && startGame.key == 39) { gamePlayer.speedX = 30; }
+//    //        if (startGame.key && startGame.key == 38) { gamePlayer.speedY = -30; }
+//    //        if (startGame.key && startGame.key == 40) { gamePlayer.speedY = 30; }
+//    //    }
+//    //} else {
+//        startGame.clear();
+//        gameWall.update();
+//        gamePlayer.speedX = 0;
+//        gamePlayer.speedY = 0;
+//        if (startGame.key && startGame.key == 37) { gamePlayer.speedX = -30; }
+//        if (startGame.key && startGame.key == 39) { gamePlayer.speedX = 30; }
+//        if (startGame.key && startGame.key == 38) { gamePlayer.speedY = -30; }
+//        if (startGame.key && startGame.key == 40) { gamePlayer.speedY = 30; }
+
+//        gamePlayer.newPos();
+//        gamePlayer.update();
+//    //}
+//}
+//function moveUp() {
+//    gamePlayer.speedY -= 30;
+//} 
+
+//function moveDown() {
+//    gamePlayer.speedY += 30;
+//}
+
+//function moveLeft() {
+//    gamePlayer.speedX -= 30;
+//}
+
+//function moveRight() {
+//    gamePlayer.speedX += 30;
+//}
+//function stopMove() {
+//    gamePlayer.speedX = 0;
+//    gamePlayer.speedY = 0;
+//}
+//function Collide() {
+//    while (true) {
+//        if (!collide(gamePlayer, gameWall))
+//            gamePlayer.x++;
+//    }
+//}
+////function Controll() {
+////    while (Continue == true) {
+////        if()
+////    }
+////}
--- Real.js
+++ Real.js
@@ -1,9 +1,17 @@
 // JavaScript source code
 
 var block = [];
+var goal = [];
+var player;
+
+var goalBlocks = 0;
+
+var ctx;
+var currentMap = 0;
 
 function startGame() {
     Soko.start();
+    console.log("bla");
 }
 
 var Soko = {
@@ -16,28 +24,38 @@ var Soko = {
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateSoko, 20);
 
+
         for (index = 0; index < mapsArray[currentMap].mapGrid.length; index++) {
-            for (index2 = 0; index2 > mapsArray[currentMap].mapGrid[index].length; index2++) {
-                switch (mapsArray[currentMap].mapGrid[index][index2].ToString) {
+            for (index2 = 0; index2 < mapsArray[currentMap].mapGrid[index].length; index2++) {
+                switch (mapsArray[currentMap].mapGrid[index][index2].toString()) {
                     case "W":
-                        block.push(new Blocks(30, 30, "green", index2 * 30, index * 30, index2, indeex, "wall"));
+                        block.push(new Blocks(30, 30, "green", index2 * 30, index * 30, index2, index, "wall"));
+                        console.log("1");
                         break;
                     case "G":
+                        goal.push(new Blocks(30, 30, "orange", index2 * 30, index * 30, index2, index, "goal"));
+                        goalBlocks += 1;
+                        console.log("2");
                         break;
                     case "B":
+                        block.push(new Blocks(30, 30, "blue", index2 * 30, index * 30, index2, index, "block"));
+                        console.log("3");
                         break;
                     case "P":
+                        player = new PlayerBlock(30, 30, "red", index2 * 30, index * 30, "player");
+                        console.log("4");
                         break;
                     default:
                         break;
                 }
+               
             }
         }
     },
     clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
-}
+};
 class Blocks {
     constructor(width, height, color, x, y, indexX, indexY, type) {
     this.width = width;
@@ -49,13 +67,47 @@ class Blocks {
     this.positionY = indexY;
     this.type = type;
     this.update = function () {
-    ctx = Soko.context;
-    ctx.fillSyle = color;
-    ctx.fillRect(this.x, this.y, this.color, this.x, this.y, this.index, this.index2, this.type);
-    }
+        ctx = Soko.context;
+        ctx.fillStyle = color;
+        //ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    };
     }
 }
+class PlayerBlock {
+    constructor(width, height, color, x, y, type) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.x = x;
+        this.y = y;
+        this.type = type;
+        this.update = function () {
+            ctx = Soko.context;
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        };
+}
+}
+//function component(width, height, color, x, y) {
+//    this.width = 30;
+//    this.height = 30;
+//    this.color = color;
+//    this.x = x;
+//    this.update = function () {
+//        ctx = Soko.context;
+//        ctx.fillSyle = color;
+//        ctx.fillRect(this.x, this.y, this.color, this.width, this.height);
+//    };
+//}
 function updateSoko() {
     Soko.clear();
-    gameWall.update();
+    for (i = 0; i < block.length; i++) {
+        block[i].update();
+    }
+    for (i = 0; i < goal.length; i++) {
+        goal[i].update();
+    }
+    player.update();
+    //gameWall.update();
 }
